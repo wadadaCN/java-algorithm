bd585a3da87e9a2cebdf418edc7fc3ee09d9d5c2
wadadaCN <xie_kun@163.com>
2018-06-24 11:55:50 +0800

    A[0]存储heapSize，数组下标从1开始，更直观

--- Charpet6/heapSort.java
+++ Charpet6/heapSort.java
@@ -1,15 +1,14 @@
 /**
-* @version 1.00 2018.6.16
+* @version 2.00 2018.6.24
 * @author xiekun
 */
 
 public class heapSort{
     public static void main(String[] args){
-        //Todo
-        //int[] A = {16, 4, 10, 14, 7, 9, 3, 2, 8, 1};
-        //maxHeapifyWithWhile(A, 1);
-        int[] A = {4, 1, 3, 2, 16, 9, 10, 14, 8, 7};
-        //buildMaxHeap(A);
+        int[] B = {4, 1, 3, 2, 16, 9, 10, 14, 8, 7};
+        int[] A = new int[B.length + 1];
+        A[0] = B.length;
+        for (int i = 0;i < B.length;i++) A[i + 1] = B[i];
         heapSort(A);
         for (int i = 0;i < A.length;i++) System.out.println(A[i]);
     }
@@ -26,27 +25,29 @@ public class heapSort{
         return i * 2 + 1;
     }
     
-    private static void maxHeapify(int[] A, int i, int heapSize){
-        int l = left(i + 1);
-        int r = right(i + 1);
+    private static void maxHeapify(int[] A, int i){
+        int l = left(i);
+        int r = right(i);
         int largest = i;
-        if (l <= heapSize && A[l - 1] > A[i]) largest = l - 1;
-        if (r <= heapSize && A[r - 1] > A[largest]) largest = r - 1;
+        int heapSize = A[0];
+        if (l <= heapSize && A[l] > A[i]) largest = l;
+        if (r <= heapSize && A[r] > A[largest]) largest = r;
         if (largest != i){
             int temp = A[largest];
             A[largest] = A[i];
             A[i] = temp;
-            maxHeapify(A, largest, heapSize);
+            maxHeapify(A, largest);
         }
     }
     
-    private static void maxHeapifyWithWhile(int[] A, int i, int heapSize){
-        while (i <= heapSize/2 -1){
-            int l = left(i + 1);
-            int r = right(i + 1);
+    private static void maxHeapifyWithWhile(int[] A, int i){
+        int heapSize = A[0];
+        while (i <= heapSize/2){
+            int l = left(i);
+            int r = right(i);
             int largest = i;
-            if (A[l - 1] > A[i]) largest = l - 1;
-            if (r <= heapSize && A[r - 1] > A[largest]) largest = r - 1;
+            if (A[l] > A[i]) largest = l;
+            if (r <= heapSize && A[r] > A[largest]) largest = r;
             if (largest != i){
                 int temp = A[largest];
                 A[largest] = A[i];
@@ -58,24 +59,22 @@ public class heapSort{
     }
     
     private static void buildMaxHeap(int[] A){
-        int heapSize = A.length;
         for (int i = Math.floorDiv(A.length, 2);i >= 1;i--){
-            maxHeapify(A, i - 1, heapSize);
+            maxHeapifyWithWhile(A, i);
         }
     }
     
     public static void heapSort(int[] A){
         buildMaxHeap(A);
-        int heapSize = A.length;
-        for (int i = A.length;i > 1;i--){
-            int temp = A[i - 1];
-            A[i - 1] = A[0];
-            A[0] = temp;
-            heapSize--;
-            maxHeapify(A, 0, heapSize);
+        int heapSize = A[0];
+        for (int i = heapSize;i > 1;i--){
+            int temp = A[i];
+            A[i] = A[1];
+            A[1] = temp;
+            A[0]--;
+            maxHeapify(A, 1);
         }
     }
-}
 
 
 
