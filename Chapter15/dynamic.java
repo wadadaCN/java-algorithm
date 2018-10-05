a0b9b5ca852fc815a7f5301fbffc6d588cc212a9
wadadaCN <xie_kun@163.com>
2018-10-05 22:01:16 +0800

    Update dynamic.java
    增加矩阵链乘法

--- Chapter15/dynamic.java
+++ Chapter15/dynamic.java
@@ -22,6 +22,11 @@ public class dynamic{
 		System.out.println(bottomUpCutRod(price, price.length - 1));
 		endTime = System.nanoTime();
 		System.out.println("动态规划运行时间：" + (endTime - startTime) + "ns");
+		
+		
+		int[] p = {30, 35, 15, 5, 10, 20, 25};
+		int[][] s = matrixChainOrder(p);
+		printOptimalParens(s, 1, 6);
 	}
 	
 	public static int cutRod(int[] price, int n){
@@ -52,4 +57,69 @@ public class dynamic{
 		System.out.println();
 		return result[n];
 	}
+	
+	public static int[][] matrixChainOrder(int[] p){
+		int n = p.length - 1;
+		int[][] m = new int[p.length][p.length];
+		int[][] s = new int[p.length][p.length];
+		for (int i = 1;i <= n;i++){
+			m[i][i] = 0;
+		}
+		for (int l = 2;l <= n;l++){
+			for (int i = 1;i <= n - l + 1;i++){
+				int j = i + l - 1;
+				m[i][j] = 2147483647;
+				for (int k = i;k <= j - 1;k++){
+					int q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
+					if (q < m[i][j]){
+						m[i][j] = q;
+						s[i][j] = k;
+					}
+				}
+			}
+		}
+		return s;
+	}
+	//根据输出的乘法结合律方案可以构造矩阵乘法顺序
+	public static void printOptimalParens(int[][] s, int i, int j){
+		if (i == j) System.out.print("A" + i);
+		else{
+			System.out.print("(");
+			printOptimalParens(s, i, s[i][j]);
+			printOptimalParens(s, s[i][j] + 1, j);
+			System.out.print(")");
+		}
+	}
+	//untest
+	public static int[][] matrixMultiply(int[][] A, int[][] B){
+		if (A[0].length != B.length) new Exception("incompatible dimentions");
+		else{
+			int[][] C = new int[A.length][B[0].length];
+			for (int i = 0;i < A.length;i++){
+				for (int j = 0;j < B[0].length;j++){
+					for (int k = 0;k < A[0].length;k++){
+						c[i][j] += A[i][k] * B[k][j];
+					}
+				}
+			}
+		}
+		return C;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
